f43498503e979ea0a41c2246d9489c4d402e86a2
Brian Sturgis <sturujisu@gmail.com>
2020-10-23 15:05:34 -0700

    labels variables in the index.js file

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
-$(document).readyState(function)() {
-  $()
-}
+$(document).ready(function() {
+    let artmath = $("#question1").val();
+    console.log(artmath)
+    let designtheory = $("#question2").val();
+    console.log(designtheory)
+    let money = $("#question3").val();
+    console.log(money)
+    let job = $("#question4").val();
+    console.log(job)
+    let drink = $("#question5").val();
+    console.log(drink)
+
+
+
+    event.preventDefault();
+    event.preventDefault();
+
+    });
+});
