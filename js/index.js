96486e811bb9035566486c2aceeb7f87db295773
Brian Sturgis <sturujisu@gmail.com>
2020-10-23 19:15:05 -0700

    beginning of the night shift 1st commit

--- js/index.js
+++ js/index.js
@@ -1,15 +1,2 @@
-$(document).ready(function() {
-  $("form#question").submit(function(event)
-  let artmath = $("#question1").val();
-    console.log(artmath)
-  let designtheory = $("#question2").val();
-    console.log(designtheory)
-    let money = $("#question3").val();
-    console.log(money)
-    let job = $("#question4").val();
-    console.log(job)
-    let drink = $("#question5").val();
-    console.log(drink)
-
-});
-
+(document).event
+console.log("hi")
