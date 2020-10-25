737d5df01413c1b3c26bbe72bcb7f472b8a17d47
Brian Sturgis <sturujisu@gmail.com>
2020-10-25 16:31:33 -0700

    closes work on this for today

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
 
-$("form#questions").submit(function(event) {
-var q1 =$("#question1")
-var x = 10;
-var y = 5;
-
-if (q1 == "math") {
-  console.log ("if")
-}
+$(document).ready(function() {
+  $("form#questions").submit(function(event) {
+    var q1 = $("#question1").val();
+    var q2 = $("#question2").val();
+    var q3 = $("#question3").val();
+    var q4 = $("#question4").val();
+    var q5 = $("#question5").val();
+  
+  if (q1 == "math")
+  
+  console.log("hi")
+    
+  });
+});
