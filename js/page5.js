7ed0f1f651a5aa98acc3a43167c699ce2b89160f
Brian Sturgis <sturujisu@gmail.com>
2020-10-24 19:25:40 -0700

    the first commit of the day for this application

---
+++ js/page5.js
@@ -0,0 +1,14 @@
+function add(number1, number2) {
+  return number1 / number2;
+}
+
+const number1 = parseInt(prompt("enter a fucking number dickface"));
+const number2 = parseInt(prompt("juuust one more to go"));
+const result = add(number1, number2)
+
+alert(result);
+
+
+
+
+// console.log("hi")
