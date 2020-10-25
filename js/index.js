919da986e28eef5870e16a78631167c32adcfc58
Brian Sturgis <sturujisu@gmail.com>
2020-10-25 15:22:52 -0700

    adds sample code to test and learn from to page3.html

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
-$(document).ready(function() {
-  $("form#questions").submit(function(event) 
 
-  var q1 = ("#question1").valueOf()); 
-  console.log (info);
-  }
-});
+$("form#questions").submit(function(event) {
+var q1 =$("#question1")
+var x = 10;
+var y = 5;
+
+if (q1 == "math") {
+  console.log ("if")
+}
--- js/page3.js
+++
@@ -1,1 +0,0 @@
-const beverage = $("#beverage").val();
