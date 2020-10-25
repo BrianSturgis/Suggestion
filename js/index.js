09caeb77c01f274841fea01f12e72a3e13ada809
Brian Sturgis <sturujisu@gmail.com>
2020-10-25 13:55:22 -0700

    updates the Index.html file

--- js/index.js
+++ js/index.js
@@ -1,9 +1,2 @@
-($(document).ready(function() {
-    const Q1 = $(     )
-    const Q2 = $(     )
-    const Q3 = $(     )
-    const Q4 = $(     )
-    const Q5 = $(     )
-
-    if (Q1 == )
-  });   
+const q1 = "dan"
+console.log(q1)
---
+++ js/page4.js
@@ -0,0 +1,9 @@
+let bunnyName = "Flopsy";
+
+function hippityHoppity() {
+  alert(bunnyName);
+  bunnyName="Cottontail";
+}
+
+hippityHoppity();
+alert(bunnyName);
