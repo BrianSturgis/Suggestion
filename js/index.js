d226cbef1697f1558c9ecc1428f30344c4e6155a
Brian Sturgis <sturujisu@gmail.com>
2020-10-25 19:58:49 -0700

    will center the text and add another answer to the question

--- js/index.js
+++ js/index.js
@@ -13,21 +13,27 @@ $(document).ready(function(event) {
     // console.log("q5")
     var answer1 = $("*").val();
     // console.log(answer1)
+    
 
+  if (q5 == "ohhh nooo"){
+    $("#rabits").toggle().show();
 
-  if (q2 == "theory" || q1 == "math"){
-    alert("answer1");
-    console.log("answer 1");
+  }
+  else if (q2 == "theory" || q1 == "math"){
+    $("#ruby").toggle().show();
+    // console.log("answer 1");
   
-  } else if (q2 == "art" || q1 == "design"){
-    alert("answer2");
-    console.log("answer 1");
+  } else (q1 == "art" || q2 == "design")
+    $("#csharp").toggle().show();
+    // console.log("answer 1");
   
-  } else if ("*"){
-  alert("please fill in all the answers");
+  // } else ("*")
+  // alert("please fill in all the answers");
+  // event.preventDefault();
 
-  } else ("")
-  alert("try again")
+  // } else ("ohhh nooo") 
+  //   $("rabits").toggle().show();
+  
   event.preventDefault();
     
   });
