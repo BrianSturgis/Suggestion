3f6bbc992f68cd964fb23801dfe088dddd44f634
Brian Sturgis <sturujisu@gmail.com>
2020-10-25 18:35:31 -0700

    add finished JS code to shoose answer

--- js/index.js
+++ js/index.js
@@ -1,15 +1,34 @@
 
-$(document).ready(function() {
+$(document).ready(function(event) {
   $("form#questions").submit(function(event) {
     var q1 = $("#question1").val();
+    // console.log("q1")
     var q2 = $("#question2").val();
+    // console.log("q2")
     var q3 = $("#question3").val();
+    // console.log("q3")
     var q4 = $("#question4").val();
+    // console.log("q4")
     var q5 = $("#question5").val();
+    // console.log("q5")
+    var answer1 = $("*").val();
+    // console.log(answer1)
+
+
+  if (q2 == "theory" || q1 == "math"){
+    alert("answer1");
+    console.log("answer 1");
   
-  if (q1 == "math")
+   } else if (q2 == "art" || q1 == "design"){
+    alert("answer2");
+    console.log("answer 1");
   
-  console.log("hi")
+  } else if ("*"){
+  alert("please fill in all the answers");
+
+  } else ("")
+  alert("try again")
+  event.preventDefault();
     
   });
 });
